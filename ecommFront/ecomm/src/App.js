ab157f8a5bfc504d8ba9937061311ca1e65f1828
SlavKing <aliosseili2001@live.ca>
2023-11-04 17:25:21 -0400

    Screens, Routing
    Added screens,
    Added react-router-dom

--- ecommFront/ecomm/src/App.js
+++ ecommFront/ecomm/src/App.js
@@ -1,26 +1,41 @@
-import logo from "./img/logo.svg";
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
     return (
-        <div className="App">
-            <div class="navbar">
-                <a href="#">
-                    <img src={logo} alt="logo" />
-                </a>
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
+        <BrowserRouter>
+            <div className="App">
+                <div className="navbar">
+                    <a href="/">
+                        <img
+                            src={window.location.origin + "/img/logo.svg"}
+                            alt="logo"
+                        />
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/">Blog</a>
+                        </li>
+                        <li>
+                            <a href="/">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="main">
+                    <Routes>
+                        <Route
+                            path="/product/:slug"
+                            element={<ProductScreen />}
+                        />
+                        <Route path="/" element={<HomeScreen />} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
