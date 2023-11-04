906772c7783dfc72ed8e864600270ca6a8e982c3
SlavKing <aliosseili2001@live.ca>
2023-11-04 18:12:50 -0400

    Shifting to bootstrap UI
    Added bootstrap to commence use of bootstrap styling

--- ecommFront/ecomm/src/App.js
+++ ecommFront/ecomm/src/App.js
@@ -2,38 +2,45 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+
 function App() {
     return (
         <BrowserRouter>
-            <div className="App">
-                <div className="navbar">
-                    <a href="/">
-                        <img
-                            src={window.location.origin + "/img/logo.svg"}
-                            alt="logo"
-                        />
-                    </a>
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/">Blog</a>
-                        </li>
-                        <li>
-                            <a href="/">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="main">
-                    <Routes>
-                        <Route
-                            path="/product/:slug"
-                            element={<ProductScreen />}
-                        />
-                        <Route path="/" element={<HomeScreen />} />
-                    </Routes>
-                </div>
+            <div className="d-flex flex-column site-container">
+                <header>
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                            <LinkContainer to="/">
+                                <Navbar.Brand>
+                                    <img
+                                        src={
+                                            window.location.origin +
+                                            "/img/logo.svg"
+                                        }
+                                        alt="logo"
+                                    />
+                                </Navbar.Brand>
+                            </LinkContainer>
+                        </Container>
+                    </Navbar>
+                </header>
+                <main>
+                    <Container>
+                        <Routes>
+                            <Route
+                                path="/product/:slug"
+                                element={<ProductScreen />}
+                            />
+                            <Route path="/" element={<HomeScreen />} />
+                        </Routes>
+                    </Container>
+                </main>
+                <footer>
+                    <div className="text-center"> All rights reserved</div>
+                </footer>
             </div>
         </BrowserRouter>
     );
