3851e821745203bbcb09496ed0eaa593aa1efa7b
SlavKing <aliosseili2001@live.ca>
2023-11-04 14:36:04 -0400

    Header commit
    Creation of basic header from my header demo.

--- ecommFront/ecomm/src/App.js
+++ ecommFront/ecomm/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div class="navbar">
+                <a href="#">
+                    <img src={logo} alt="logo" />
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
