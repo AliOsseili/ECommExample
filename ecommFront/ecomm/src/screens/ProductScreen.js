340b686d4fbd7fcee30696f22fa3b7f4a2571139
SlavKing <aliosseili2001@live.ca>
2023-11-05 20:21:59 -0500

    Further UI Improvements
    Made loading spinner and message boxes for displaying errors.
    
    Implemented utils.js to contain helper functions.

--- ecommFront/ecomm/src/screens/ProductScreen.js
+++ ecommFront/ecomm/src/screens/ProductScreen.js
@@ -12,6 +12,9 @@ import Button from "react-bootstrap/esm/Button";
 import Rating from "../components/Rating";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 const reducer = (state, action) => {
     switch (action.type) {
         case "FETCH_REQUEST":
@@ -40,15 +43,15 @@ function ProductScreen() {
                 const result = await axios.get(`/api/products/slug/${slug}`);
                 dispatch({ type: "FETCH_SUCCESS", payload: result.data });
             } catch (error) {
-                dispatch({ type: "FETCH_FAIL", payload: error.message });
+                dispatch({ type: "FETCH_FAIL", payload: getError(error) });
             }
         };
         fetchData();
     }, [slug]);
     return loading ? (
-        <div>Loading...</div>
+        <LoadingBox />
     ) : error ? (
-        <div>{error}</div>
+        <MessageBox variant="danger">{error}</MessageBox>
     ) : (
         <div>
             <Row>
