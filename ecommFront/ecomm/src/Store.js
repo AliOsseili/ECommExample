dfb3f1522160d251ee3b242ad958e6c356cd7e2d
SlavKing <aliosseili2001@live.ca>
2023-11-06 14:51:45 -0500

    Cart Functioniality
    Implemented cart functionality.

--- ecommFront/ecomm/src/Store.js
+++ ecommFront/ecomm/src/Store.js
@@ -2,7 +2,9 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 const initialState = {
     cart: {
-        cartItems: [],
+        cartItems: localStorage.getItem("cartItems")
+            ? JSON.parse(localStorage.getItem("cartItems"))
+            : [],
     },
 };
 
@@ -18,6 +20,7 @@ function reducer(state, action) {
                       item._id === existItem._id ? newItem : item
                   )
                 : [...state.cart.cartItems, newItem];
+            localStorage.setItem("cartItems", JSON.stringify(cartItems));
 
             return {
                 ...state,
@@ -26,6 +29,14 @@ function reducer(state, action) {
                     cartItems,
                 },
             };
+
+        case "CART_REMOVE_ITEM": {
+            const cartItems = state.cart.cartItems.filter(
+                (item) => item._id !== action.payload._id
+            );
+            localStorage.setItem("cartItems", JSON.stringify(cartItems));
+            return { ...state, cart: { ...state.cart, cartItems } };
+        }
 
         default:
             return state;
