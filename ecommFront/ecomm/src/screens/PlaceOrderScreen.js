07a532df4cc234cdf51539be1f257fa6ef5be4eb
SlavKing <aliosseili2001@live.ca>
2023-11-08 18:09:17 -0500

    Implement Order Backend and Functionality
    POST to backend order items. Clear cart afterwards and display proper UI elements.
    
    Backend verifies user via bearer token to ensure user is a logged in person. Does through middleware function isAuth.
    
    Setting up for payment result as well in the orderMordel.

--- ecommFront/ecomm/src/screens/PlaceOrderScreen.js
+++ ecommFront/ecomm/src/screens/PlaceOrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useNavigate } from "react-router-dom";
 import Row from "react-bootstrap/Row";
@@ -7,10 +7,31 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Store } from "../Store";
+import Axios from "axios";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
 import CheckoutSteps from "../components/CheckoutSteps";
+import LoadingBox from "../components/LoadingBox";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "CREATE_REQUEST":
+            return { ...state, loading: true };
+        case "CREATE_SUCCESS":
+            return { ...state, loading: false };
+        case "CREATE_FAIL":
+            return { ...state, loading: false };
+        default:
+            return state;
+    }
+};
 
 export default function PlaceOrderScreen() {
     const navigate = useNavigate();
+    const [{ loading }, dispatch] = useReducer(reducer, {
+        loading: false,
+    });
+
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const { cart, userInfo } = state;
 
@@ -22,8 +43,36 @@ export default function PlaceOrderScreen() {
     cart.taxPrice = round2(0.13 * cart.itemsPrice);
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
 
-    const placeOrderHandler = async () => {};
+    const placeOrderHandler = async () => {
+        try {
+            dispatch({ type: "CREATE_REQUEST" });
 
+            const { data } = await Axios.post(
+                "/api/orders",
+                {
+                    orderItems: cart.cartItems,
+                    shippingAddress: cart.shippingAddress,
+                    paymentMethod: cart.paymentMethod,
+                    itemsPrice: cart.itemsPrice,
+                    shippingPrice: cart.shippingPrice,
+                    taxPrice: cart.taxPrice,
+                    totalPrice: cart.totalPrice,
+                },
+                {
+                    headers: {
+                        authorization: `Bearer ${userInfo.token}`, //hacker deterence
+                    },
+                }
+            );
+            ctxDispatch({ type: "CART_CLEAR" });
+            dispatch({ type: "CREATE_SUCCESS" });
+            localStorage.removeItem("cartItems");
+            navigate(`/order/${data.order._id}`);
+        } catch (err) {
+            dispatch({ type: "CREATE_FAIL" });
+            toast.error(getError(err));
+        }
+    };
     useEffect(() => {
         if (!cart.paymentMethod) {
             navigate("/payment");
@@ -48,7 +97,7 @@ export default function PlaceOrderScreen() {
                                 <strong>Address: </strong>{" "}
                                 {cart.shippingAddress.address},
                                 {cart.shippingAddress.city},{" "}
-                                {cart.shippingAddress.postalCode},
+                                {cart.shippingAddress.postalcode},
                                 {cart.shippingAddress.country}
                             </Card.Text>
                             <Link to="/shipping">Edit</Link>
@@ -145,6 +194,7 @@ export default function PlaceOrderScreen() {
                                             Place Order
                                         </Button>
                                     </div>
+                                    {loading && <LoadingBox></LoadingBox>}
                                 </ListGroup.Item>
                             </ListGroup>
                         </Card.Body>
